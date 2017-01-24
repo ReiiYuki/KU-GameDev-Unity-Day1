10c13c3270aa10beb47ef016ed70a899f4f2f11e
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-24 10:57:57 +0700

    Turn on/off light button

---
+++ Assets/SwtichControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwtichControl : MonoBehaviour {
+
+    public GameObject sun;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width/2-150/2, Screen.height/2-150/2, 150, 150), "Switch"))
+            sun.GetComponent<Light>().enabled = !sun.GetComponent<Light>().enabled;
+    }
+}
