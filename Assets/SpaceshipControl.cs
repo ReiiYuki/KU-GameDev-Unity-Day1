4342aefd2e9cf0271b5878014424df3a2d1e7095
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-01-24 17:09:08 +0700

    Idiot Spaceship

---
+++ Assets/SpaceshipControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipControl : MonoBehaviour {
+    Rigidbody physicObject;
+    Vector3 leftForce;
+    Vector3 rightForce;
+	// Use this for initialization
+	void Start () {
+        physicObject = GetComponent<Rigidbody>();
+        leftForce = new Vector3(10,0);
+        rightForce = new Vector3(-10, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            this.gameObject.transform.Translate(Input.GetAxis("Horizontal") * Vector3.right * Time.deltaTime *10);
+            Debug.Log(this.gameObject.transform.position);
+            
+        }
+    }
+}
